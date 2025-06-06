8e5b4e795cf4c4fbcdcc984886dbffe5397f146c
Opposite34 <54463205+Opposite34@users.noreply.github.com>
2025-06-06 16:01:53 +0900

    lowercase package

--- pkgs/furnace-appimage.nix
+++ pkgs/furnace-appimage.nix
@@ -1,10 +1,9 @@
 { pkgs, ... }: let
-  pname = "Furnace";
-  pnameLower = pkgs.lib.toLower pname;
+  pname = "furnace";
   version = "0.6.8.2";
 
   src = pkgs.fetchurl {
-    url = "https://github.com/tildearrow/furnace/releases/download/v${version}/${pname}-${version}-x86_64.AppImage";
+    url = "https://github.com/tildearrow/furnace/releases/download/v${version}/Furnace-${version}-x86_64.AppImage";
     hash = "sha256-kkEjCAomTufBRk+V8tvnVySDYvtbNMQXr5R6yHEQDgw=";
   };
   appimageContents = pkgs.appimageTools.extract {inherit pname version src;};
@@ -14,7 +13,7 @@ pkgs.appimageTools.wrapType2 {
   pkgs = pkgs;
   extraInstallCommands = ''
     install -m 444 -D \
-    ${appimageContents}/${pnameLower}.desktop -t $out/share/applications
+    ${appimageContents}/${pname}.desktop -t $out/share/applications
     cp -r ${appimageContents}/usr/share/icons $out/share
   '';
 }
